0453633b0e5d0a9315aefc8d6394c346384a6b47
Maxwell Farias <maxwell.1nefi37@gmail.com>
2022-01-24 04:34:00 -0300

    up

---
+++ NetflixRemake/app/src/main/java/com/example/netflixremake/model/Movie.java
@@ -0,0 +1,14 @@
+package com.example.netflixremake.model;
+
+public class Movie {
+    //Faz referencias a arquvos da parta res
+    int corver;
+
+    public int getCorver() {
+        return corver;
+    }
+
+    public void setCorver(int corver) {
+        this.corver = corver;
+    }
+}
