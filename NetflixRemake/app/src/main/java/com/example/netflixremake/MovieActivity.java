a24074b8cf7a8161297790e2c47c324decc130c9
Maxwell Farias <maxwell.1nefi37@gmail.com>
2022-01-27 20:05:21 -0300

    Capa do Filme Com Sombras - aula concluida

--- NetflixRemake/app/src/main/java/com/example/netflixremake/MovieActivity.java
+++ NetflixRemake/app/src/main/java/com/example/netflixremake/MovieActivity.java
@@ -2,8 +2,13 @@ package com.example.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -24,6 +29,21 @@ public class MovieActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             //Adiciona a seta para voltar
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        }
+
+        //Metodo para manipular um drawble, nao foi usado apenas o getDrawble pois o ContextCompat tem metodos com mais compatibilidade a dispositivos antigos
+        //Foi feito um cast para (LayerDrawable), pois o retorno do ContextCompat.getDrawable seria apenas um drawable
+        LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+
+        //Verifica se eh diferente de null para nao dar nullpointer no codigo
+        if (drawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie);
+
+            //Acessando o item das sombras e trocando a sua propriedade
+            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+
+            //Acessa o ImageView do movieItem.xlm e troca a imagem de fundo.
+            ((ImageView) findViewById(R.id.image_view_cover)).setImageDrawable(drawable);
         }
     }
 }
