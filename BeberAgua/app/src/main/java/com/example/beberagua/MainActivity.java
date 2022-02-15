16b0024d0c89690c0f63d65885d8875149aaf2e7
Maxwell Farias <maxwell.1nefi37@gmail.com>
2022-02-15 04:51:36 -0300

    (1.0) Cancelando Alarmes  - Aula concluida

--- BeberAgua/app/src/main/java/com/example/beberagua/MainActivity.java
+++ BeberAgua/app/src/main/java/com/example/beberagua/MainActivity.java
@@ -88,7 +88,8 @@ public class MainActivity extends AppCompatActivity {
             //Confirm changes in db
             editor.apply();
 
-            /*Esse objeto foi criado a fim de colocar as informacoes da hora e minutos do vindos da marcacao do timerPicker, em seguida esse tempo sera convertido
+            /*Esse objeto foi criado a fim de colocar as informacoes da hora e minutos do vindos da marcacao do timerPicker, em seguida esse tempo
+            sera convertido
             em milisegundos*/
             Calendar calendar = Calendar.getInstance();
             calendar.set(Calendar.HOUR_OF_DAY, hour);
@@ -103,22 +104,24 @@ public class MainActivity extends AppCompatActivity {
             notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION, "Hora de beber água");
             /*Criando o broadcast (que eh uma intencao pendente). Os broadcasts sao recursos de transmissao de dados em background-> context, requisicao
              ID (quem eh quem esta chamando), intencao (nesse caso uma intencao de notification), flag
-             com um metodo de atualizacao, nesse caso FLAG_CANCEL_CURRENT o qual indica que se ja existir uma atual pendingItent, esta deve ser cancelada antes
-            de gerar uma nova*/
-            PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+             com um metodo de atualizacao, nesse caso FLAG_CANCEL_CURRENT o qual indica que se ja existir uma atual pendingItent, esta deve ser cancelada
+              antes de gerar uma nova*/
+            PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 0, notificationIntent,
+                    PendingIntent.FLAG_CANCEL_CURRENT);
 
 
             //Pegando o alarme para manipula-lo
             AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            /*Configura as informacoes da repeticao em um horario "nao tao exato", essa repeticao acha o melhor horario para disparar a fim de nao sobrecarregar o
-            sitema do android.
-            * Parametros: AlarmManager.RTC_WAKEUP-> Manda acordar o celular; hora de disparo em milisegundos; intervalo de disparo; Operacao em si*/
-            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), interval * 1000, broadcast);
+            /*Configura as informacoes da repeticao em um horario "nao tao exato", essa repeticao acha o melhor horario para disparar a fim de nao
+            sobrecarregar o sitema do android.
+            * Parametros: AlarmManager.RTC_WAKEUP-> Manda acordar o celular; hora de disparo em milisegundos (x60 para conveter os minutos no interval em
+            segundos); intervalo de disparo; Operacao em si*/
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), interval * 60 * 1000, broadcast);
 
 
         } else {
-            //Se em outra parte do codigo for mudado a cor desse mesmo botao, sera conservado o drawble, e sera mudado somente a sua cor, nao mostrando mais
-            //a cor do drawble
+            //Se em outra parte do codigo for mudado a cor desse mesmo botao, sera conservado o drawble, e sera mudado somente a sua cor, nao mostrando
+            // mais a cor do drawble
             btnNotify.setBackgroundResource(R.drawable.bg_button_background_accent);
 
             btnNotify.setText(R.string.btn_notify_text);
@@ -130,6 +133,15 @@ public class MainActivity extends AppCompatActivity {
             editor.remove("hour");
             editor.remove("minute");
             editor.apply();
+
+            //Cancelando as notificacoes do aplicativo
+            Intent notificationIntent = new Intent (MainActivity.this, NotificationPublisher.class);
+            PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 0, notificationIntent,
+                    PendingIntent.FLAG_CANCEL_CURRENT);
+            AlarmManager alarmManager = (AlarmManager) getSystemService (Context.ALARM_SERVICE);
+            alarmManager.cancel(broadcast);
+
+
         }
 
         Log.d("test", "Hour: " + hour + " Minute: " + minute + " Inteval: " + interval);
