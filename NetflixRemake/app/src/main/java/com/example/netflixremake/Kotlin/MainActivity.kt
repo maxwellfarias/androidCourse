a7d218dd540fc66c5408cb93c7b6dca6a312b18e
Maxwell Farias <maxwell.1nefi37@gmail.com>
2022-03-15 14:12:41 -0300

    Commit com erros - prestes a dar um revert

---
+++ NetflixRemake/app/src/main/java/com/example/netflixremake/Kotlin/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.netflixremake.Kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.netflixremake.MainActivity
+
+class MainActivity : MainActivity() {
+    // Os ':' em Kotlin substitue o extends e o implements
+}
