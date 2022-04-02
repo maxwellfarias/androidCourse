cab4289e45cad2f827e99f37d601a4cba0177eb7
Maxwell Farias <maxwell.1nefi37@gmail.com>
2022-04-02 04:54:11 -0300

    object Kotlin e Retrofit Framework p2 - Aula concluida

--- NetflixRemake/app/src/main/java/com/example/netflixremake/kotlin/MovieActivity.kt
+++ NetflixRemake/app/src/main/java/com/example/netflixremake/kotlin/MovieActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -19,12 +20,16 @@ import com.bumptech.glide.request.target.DrawableImageViewTarget
 import com.bumptech.glide.request.target.Target
 import com.example.netflixremake.R
 import com.example.netflixremake.model.Movie
+import com.example.netflixremake.model.MovieDetail
 import com.example.netflixremake.util.ImageDownloadTask
 import com.example.netflixremake.util.MovieDetailTask
 import kotlinx.android.synthetic.main.activity_movie.*
 import kotlinx.android.synthetic.main.category_item.*
 
 import kotlinx.android.synthetic.main.movie_item_similar.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MovieActivity : AppCompatActivity() {
     private lateinit var movieSimilarAdapter: MovieSimilarAdapter
@@ -38,67 +43,84 @@ class MovieActivity : AppCompatActivity() {
         intent.extras?.let {
             val id = it.getInt("id")
             val task = MovieDetailTask(this)
-            task.setMovieDetailLoader { movieDetail ->
-                text_view_title_cover.text = movieDetail.title
-                text_view_cast.text = getString(R.string.cast, movieDetail.cast)
-                text_view_desc.text = movieDetail.desc
 
-                val movies = arrayListOf<Movie>()
-                movieSimilarAdapter = MovieSimilarAdapter(movies)
-                rv_similar.adapter = movieSimilarAdapter
-                rv_similar.layoutManager = GridLayoutManager(this, 3)
+            retrofit().create(NetflixAPI::class.java)
+                .getMovieBy(id) //Passa o ID para ser colocado no final do link para pegar as informacoes do movieDetail
+                .enqueue(object: Callback<MovieDetail>{
+                    override fun onFailure(call: Call<MovieDetail>, t: Throwable) {
+                        Toast.makeText(this@MovieActivity, t.message, Toast.LENGTH_SHORT).show()
+                    }
 
-                /*
-                Forma anterior de se carregar uma imagem:
-                O metodo apply faz com que possa ser chamado os metodos do imageDownloadTask sem a necessidade de instanciar o ImageDownloadTask
-                ImageDownloadTask(image_view_cover_play).apply {
-                    setShadowEnabled(true)
-                    execute(movieDetail.movie.coverUrl)
-                }*/
+                    override fun onResponse(
+                        call: Call<MovieDetail>,
+                        response: Response<MovieDetail>
+                    ) {
+                        if (response.isSuccessful){
+                            response.body()?.let {movieDetail ->
+                                text_view_title_cover.text = movieDetail.title
+                                text_view_cast.text = getString(R.string.cast, movieDetail.cast)
+                                text_view_desc.text = movieDetail.desc
 
-                //Adicionando a imagem com sombra ao fundo
-                Glide.with(this)
-                    .load(movieDetail.coverUrl)
-                    //Serve para ouvir eventos quando estiver preparado para fazer insert do bitmap e tambem para ouvir alguma falha
-                    .listener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(
-                            e: GlideException?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            return true //foi colocado true, porque nao sera renderizado nada
-                        }
+                                /*
+                                 Forma anterior de se carregar uma imagem:
+                                 O metodo apply faz com que possa ser chamado os metodos do imageDownloadTask sem a necessidade de instanciar o ImageDownloadTask
+                                 ImageDownloadTask(image_view_cover_play).apply {
+                                     setShadowEnabled(true)
+                                     execute(movieDetail.movie.coverUrl)
+                                 }*/
 
-                        //Pegua o recurso do bitmap e transforma em um drawable
-                        override fun onResourceReady(
-                            resource: Drawable?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            dataSource: DataSource?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            //Cria um drawable com as mesmas propriedades do shadows, o 'as LayerDrawable?' eh a forma que é feita o cast para LayerDrawable
-                            val drawable: LayerDrawable? = ContextCompat.getDrawable(baseContext, R.drawable.shadows) as LayerDrawable?
-                            drawable?.let {
-                                //Muda para a imagem que foi carregada atraves do link (resource faz referencia para a imagem carregada).
-                                drawable.setDrawableByLayerId(R.id.cover_drawable, resource)
-                                //target faz referencia a imageView passada no '.into()' e esta eh convertida para DrawableImageViewTarget
-                                // em seguida eh settado o drawable que foi manipulado nas linha acima
-                                (target as DrawableImageViewTarget).view.setImageDrawable(drawable)
+
+                                //Adicionando a imagem com sombra ao fundo
+                                Glide.with(this@MovieActivity)
+                                    .load(movieDetail.coverUrl)
+                                    //Serve para ouvir eventos quando estiver preparado para fazer insert do bitmap e tambem para ouvir alguma falha
+                                    .listener(object : RequestListener<Drawable> {
+                                        override fun onLoadFailed(
+                                            e: GlideException?,
+                                            model: Any?,
+                                            target: Target<Drawable>?,
+                                            isFirstResource: Boolean
+                                        ): Boolean {
+                                            return true //foi colocado true, porque nao sera renderizado nada
+                                        }
+
+                                        //Pegua o recurso do bitmap e transforma em um drawable
+                                        override fun onResourceReady(
+                                            resource: Drawable?,
+                                            model: Any?,
+                                            target: Target<Drawable>?,
+                                            dataSource: DataSource?,
+                                            isFirstResource: Boolean
+                                        ): Boolean {
+                                            //Cria um drawable com as mesmas propriedades do shadows, o 'as LayerDrawable?' eh a forma que é feita o cast para LayerDrawable
+                                            val drawable: LayerDrawable? = ContextCompat.getDrawable(baseContext, R.drawable.shadows) as LayerDrawable?
+                                            drawable?.let {
+                                                //Muda para a imagem que foi carregada atraves do link (resource faz referencia para a imagem carregada).
+                                                drawable.setDrawableByLayerId(R.id.cover_drawable, resource)
+                                                //target faz referencia a imageView passada no '.into()' e esta eh convertida para DrawableImageViewTarget
+                                                // em seguida eh settado o drawable que foi manipulado nas linha acima
+                                                (target as DrawableImageViewTarget).view.setImageDrawable(drawable)
+                                            }
+                                            return true
+                                        }
+
+                                    })
+                                    .into(image_view_cover_play)
+
+                                movieSimilarAdapter.movies.clear()
+                                movieSimilarAdapter.movies.addAll(movieDetail.moviesSimilar)
+                                movieSimilarAdapter.notifyDataSetChanged()
                             }
-                            return true
                         }
+                    }
+                })
 
-                    })
-                    .into(image_view_cover_play)
+            val movies = arrayListOf<Movie>()
+            movieSimilarAdapter = MovieSimilarAdapter(movies)
+            rv_similar.adapter = movieSimilarAdapter
+            rv_similar.layoutManager = GridLayoutManager(this, 3)
 
-                movieSimilarAdapter.movies.clear()
-                movieSimilarAdapter.movies.addAll(movieDetail.moviesSimilar)
-                movieSimilarAdapter.notifyDataSetChanged()
 
-            }
-            task.execute("https://tiagoaguiar.co/api/netflix/$id")
             //Nao precisou do findViewById, foi encontrado direto usando toolbar que eh o id da toolbar
             setSupportActionBar(toolbar)
             supportActionBar?.let { toolbar ->
--- NetflixRemake/app/src/main/java/com/example/netflixremake/kotlin/NetflixAPI.kt
+++ NetflixRemake/app/src/main/java/com/example/netflixremake/kotlin/NetflixAPI.kt
@@ -1,14 +1,21 @@
 package com.example.netflixremake.kotlin
 
 import com.example.netflixremake.model.Categories
+import com.example.netflixremake.model.MovieDetail
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface NetflixAPI {
     @GET("home")
     fun listCategories(): Call<Categories> //Define o tipo de retorno da chamada que eh a classe Categories que contem uma lista de Category
+
+    @GET ("{id}")
+    fun getMovieBy(@Path("id")id: Int): Call<MovieDetail>
+    //getMovieBy recebera como parametro um id inteiro que sera colocado no final do link para pegar as informacoes do movieDetail
+
 
 }
 //Funcao global
