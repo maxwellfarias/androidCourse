d59db0079a18806830bf4565ac2dd9c3299ef9ea
Maxwell Farias <maxwell.1nefi37@gmail.com>
2022-04-18 08:17:22 -0300

    Algoritmo Gradient Color - Aula concluida

--- joker-master/app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/presentation/HomePresenter.kt
+++ joker-master/app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/presentation/HomePresenter.kt
@@ -1,5 +1,6 @@
 package co.tiagoaguiar.tutorial.jokerappdev.presentation
 
+import android.graphics.Color
 import co.tiagoaguiar.tutorial.jokerappdev.data.CategoryRemoteDataSource
 import co.tiagoaguiar.tutorial.jokerappdev.data.ListCategoryCallback
 import co.tiagoaguiar.tutorial.jokerappdev.model.Category
@@ -29,14 +30,29 @@ class HomePresenter(
     //:::::::::::: METODO DE OUTPUT ::::::::::::: (success, falha, complete)
 
     /*A funcao eh executada quando houver sucesso na requisicao para o banco de dados, os dados serao
-    * formatadaos e enviados para a HomeFragment. onSuccess recebera como parametro uma lista de
+    * formatadaos e enviados para a HomeFragment. onSruccess recebera como parametro uma lista de
     * Strings que veio do banco de dados. Sera criado uma lista de Category com as informacoes da
     * lista do response*/
     override fun onSuccess(response: List<String>) {
+        //Sera usando uma formatacao de cores
+        val start = 40 //H: Matriz inicial
+        val end = 190 //H: Matriz final
+        val diff = (end - start)/ response.size
+
+        val categories = response.mapIndexed { index, s ->
+            val hsv = floatArrayOf(
+                start + (index * diff).toFloat(), //H
+                100f, //S
+                100f //V
+            )
+            //Sera passado um array com os tres pontos que definem uma cor segundo o padrao hsv (h: matriz,
+            // s: saturacao, v: valor) e convertido em uma cor.
+            Category(s, Color.HSVToColor(hsv).toLong())
+        }
         //Retorna uma nova lista depois que cada elemento da lista anterior foi modificado pela expressao
         // escolhida, nesse caso foi retornado uma nova lista de CategoryItem que tem como parametro o
         //cada item do Category
-        val categories = response.map { Category(it, 0xFFFF0000) }
+//        val categories = response.map { Category(it, 0xFFFF0000) }
 
         //Essa informacao sera usada pelo adapter no HomeFragment para criar os itens da fragment
         view.showCategories(categories)
